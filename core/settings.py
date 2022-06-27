488cf2fe2ab00d55f6ba28cb4ca6fb18651bda7b
faiso abdirisak <faisoabdirisak@gmail.com>
2022-06-27 22:13:16 +0300

    add

--- core/settings.py
+++ core/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'edudatabase',
+        'USER': 'faiso@edudatabase',
+        'PASSWORD': 'Faska1234',
+        'HOST': 'edudatabase.postgres.database.azure.com',
+        'PORT': '5432',
+        'OPTIONS':{'sslmode':'require'},
     }
 }
 
